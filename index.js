35806fe3db990d6a4dd3bd556037a2b8f2d112fe
Bailey Shelden <baileyshelden@Baileys-MacBook-Air-2.local>
2019-06-18 10:56:50 -0600

    initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+var quotes = [["You Deserve to be happy, You deserve to live a life you are excited about, don't let others make you forget that-Unknown", "Be happy, not because everything is good, but because you can see the good side of everything-Unknown", "If you want to be happy, BE-Leo Tolstory"],["There is no great genuis without a touch of madness-aristotle","Everyday is a fresh start-unknown", "You are only given a little spark of madness, you musn't loose it-Rei Williams"],["First, accept sadness. Accept without loosing winning isn't so great-Allisa Milano", "Tears come from the heart, and not from the brain-Leonardo Davinci", "This may be a sad chapter, but you are not a sad story-Unknown"],]
+
+
+
+// var container = $('.container');
+// var submit = $('.submit-btn');
+//
+// submit.on("click", appenduserimput);
+//
+// function appenduserimput() {
+//   var name = $('.item').val();
+//
+//   cardContainer.append(`
+//     <p class="item-card">${item} is awesome!</p>
+//   `);
+//
+//   $('.name').val("");
